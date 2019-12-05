e24bef782008ba362b3d968f1842cb9c0b2fc017
GustavoSantos16 <gustavo.santos@ddrtech.com.br>
2019-12-04 23:17:00 -0300

    Plataforma nubank v0.1

---
+++ nubank/src/components/Menu/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+// import QRCode from 'react-native-qrcode';
+
+import { Container, Code } from './styles';
+
+
+export default function Menu() {
+    return (
+        <Container>
+            <Code>
+                {/* <QRCode
+                    value=""
+                    size={80}
+                    bgColor='#FFFFFF'
+                    fgColor='#000000' /> */}
+
+
+            </Code>
+
+        </Container>
+    );
+}
